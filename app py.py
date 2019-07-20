f983dc0c4363d2cd9c56aa82c1376332b3e808f3
Jiwoo Yoon <jiwooyoon@Jiwoos-MacBook-Pro-3.local>
2019-07-20 23:51:39 +0900

    Week 2 HW submission

--- app py.py
+++ app py.py
@@ -1,21 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 articles = []
 article_no = 1
-
-@app.route('/')
-def home():
-   return render_template('Order.html')
-
-
-@app.route('/mypage')
-def mypage():
-   return render_template('index.html')
-
-#API를 주는 부분
-from flask import Flask, render_template, jsonify, request
-app = Flask(__name__)
 
 ## HTML을 주는 부분
 @app.route('/')
@@ -27,50 +14,38 @@ def mypage():
    return render_template('index.html')
 
 ## API 역할을 하는 부분
-@app.route('/test', methods=['POST'])
-def test_post():
-   global articles
-   global article_no
+@app.route('/post', methods=['POST'])
+def post():
+   global articles               # 이 함수 안에서 나오는 articles 글로벌 변수를 가리킵니다.
+   global article_no             # 이 함수 안에서 나오는 article_no는 글로벌 변수를 가리킵니다.
 
-   url_receive = request.form['url_give']
-   comment_receive = request.form['comment_give']
-   category_receive = request.form['category_give']
+   url_receive = request.form['url_give']          # 클라이언트로부터 url을 받는 부분
+   comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
+   category_receive = request.form['category_give']  # 클라이언트로부터 category를 받는 부분
 
-   article = {}
-   article['url'] = url_receive
-   article['comment'] = comment_receive
-   article['category'] = category_receive
-   article['number'] = article_no
-
-   articles.append(article)
+   article = {'url':url_receive,'comment':comment_receive, 'category':category_receive, 'no':article_no} # 받은 걸 딕셔너리로 만들고,
 
    article_no = article_no + 1
 
-   print(articles)
+   articles.append(article)   # 넣는다
 
    return jsonify({'result':'success'})
 
-@app.route('/test', methods=['GET'])
-def test_get():
-   global articles
-   return jsonify({'result':'success', 'values' :articles})
+@app.route('/post', methods=['GET'])
+def view():
+   return jsonify({'result':'success', 'articles':articles})
 
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
-
-
-
-@app.route('/delete', methods=['POST'])
+@app.route('/delete', methods=['post'])
 def delete():
-   global articles
-   global article_no
-   number_receive = request.form['number_give']
+   global articles                               # 이 함수 안에서 나오는 articles 글로벌 변수를 가리킵니다.
+   no_receive = request.form['no_give']          # 클라이언트로부터 no를 받는 부분
 
-   for article in articles:
-      if article['number'] == number_receive:
-         articles.remove(article)
+   for article in articles:                      # 반복문: articles를 돌면서,
+       if str(article['no']) == no_receive:      # 조건문: 받은 no와 같은 번호의 아티클을 찾아서 (단, 문자열 == 문자열로!)
+           articles.remove(article)              # 해당 article을 지우고,
+           return jsonify({'result': 'success'})  # 결과를 주고 함수를 끝낸다.
 
-   return jsonify({'result':'success'})
+   return jsonify({'result': 'fail', 'msg': '아티클이 없습니다'}) # 만약 반복문을 다 돌아도 결과를 주지 않았으면, 아티클이 없다고 한다.
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
